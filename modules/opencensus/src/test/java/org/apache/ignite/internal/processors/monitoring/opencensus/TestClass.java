ade17053a1a9acfe0c10b11416e7f51405599b52
Mikhail Petrov <pmgheap.sbt@gmail.com>
2020-08-25 09:03:39 +0300

    wip

---
+++ modules/opencensus/src/test/java/org/apache/ignite/internal/processors/monitoring/opencensus/TestClass.java
@@ -0,0 +1,130 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.ignite.internal.processors.monitoring.opencensus;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import io.opencensus.exporter.trace.zipkin.ZipkinExporterConfiguration;
+import io.opencensus.exporter.trace.zipkin.ZipkinTraceExporter;
+import io.opencensus.trace.Tracing;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.cache.query.FieldsQueryCursor;
+import org.apache.ignite.cache.query.SqlFieldsQuery;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.internal.IgniteEx;
+import org.apache.ignite.internal.processors.query.GridQueryProcessor;
+import org.apache.ignite.internal.util.typedef.internal.U;
+import org.apache.ignite.logger.log4j.Log4JLogger;
+import org.apache.ignite.spi.tracing.TracingConfigurationCoordinates;
+import org.apache.ignite.spi.tracing.TracingConfigurationParameters;
+import org.apache.ignite.spi.tracing.opencensus.OpenCensusTracingSpi;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.apache.ignite.spi.tracing.Scope.SQL;
+import static org.apache.ignite.spi.tracing.TracingConfigurationParameters.SAMPLING_RATE_ALWAYS;
+
+/** */
+public class TestClass {
+    /** */
+    public static final int NODES_CNT = 2;
+
+    /** */
+    public static final int TABLE_POPULATION = 2000;
+
+    /** */
+    public static final int SELECT_RANGE = 1000;
+
+    /** */
+    public static final int QRY_PAGE_SIZE = 1024;
+
+    /** */
+    public static final int UPDATE_RANGE = 100;
+
+    /** */
+    @BeforeClass
+    public static void before() {
+        ZipkinTraceExporter.createAndRegister(ZipkinExporterConfiguration.builder()
+            .setV2Url("http://localhost:9411/api/v2/spans")
+            .setServiceName("ignite")
+            .build());
+    }
+
+    /** */
+    @Test
+    public void test() throws Exception {
+        for (int i = 0; i < NODES_CNT; i++)
+            startGrid(i, false);
+
+        IgniteEx cli = startGrid(NODES_CNT, true);
+
+        GridQueryProcessor qryProc = cli.context().query();
+
+        qryProc.querySqlFields(
+            new SqlFieldsQuery("CREATE TABLE test_table (id LONG PRIMARY KEY, val LONG)"), false);
+
+        qryProc.querySqlFields(new SqlFieldsQuery("CREATE INDEX val_idx ON test_table (val)"), false);
+
+        for (long l = 1; l <= TABLE_POPULATION; ++l) {
+            qryProc.querySqlFields(
+                new SqlFieldsQuery("INSERT INTO test_table (id, val) VALUES (?, ?)").setArgs(l, l),
+                true
+            );
+        }
+
+        cli.tracingConfiguration().set(
+            new TracingConfigurationCoordinates.Builder(SQL).build(),
+            new TracingConfigurationParameters.Builder()
+                .withSamplingRate(SAMPLING_RATE_ALWAYS).build());
+
+        for (int i = 0; i < 1 ; i++) {
+            long lowId = ThreadLocalRandom.current().nextLong(TABLE_POPULATION - SELECT_RANGE);
+
+            long highId = lowId + SELECT_RANGE;
+
+            try (
+                FieldsQueryCursor<List<?>> cursor = cli
+                    .context().query().querySqlFields(
+                        new SqlFieldsQuery("SELECT id, val FROM test_table WHERE id BETWEEN ? and ?")
+                            .setArgs(lowId, highId)
+                            .setPageSize(QRY_PAGE_SIZE),
+                        false
+                    )
+            ) {
+                cursor.iterator().forEachRemaining(val -> {});
+            }
+        }
+
+        Ignition.stopAll(true);
+
+        U.sleep(5000);
+
+        Tracing.getExportComponent().shutdown();
+    }
+
+    /** */
+    private IgniteEx startGrid(int idx, boolean clientMode) throws Exception {
+        return (IgniteEx) Ignition.start(new IgniteConfiguration()
+            .setIgniteInstanceName("node-" + idx)
+            .setGridLogger(new Log4JLogger("modules/core/src/test/config/log4j-test.xml"))
+            .setTracingSpi(new OpenCensusTracingSpi())
+            .setClientMode(clientMode)
+            .setConsistentId(Integer.toString(idx)));
+    }
+}
--- modules/tracing-benchmarks/src/main/java/org/apache/ignite/SqlTracingBenchmarks.java
+++ modules/tracing-benchmarks/src/main/java/org/apache/ignite/SqlTracingBenchmarks.java
@@ -77,7 +77,7 @@ public class SqlTracingBenchmarks {
         public static final int SELECT_RANGE = 1000;
 
         /** */
-        public static final int QRY_PAGE_SIZE = 10;
+        public static final int QRY_PAGE_SIZE = 1024;
 
         /** */
         public static final int UPDATE_RANGE = 100;
@@ -107,10 +107,10 @@ public class SqlTracingBenchmarks {
                 );
             }
 
-           /* cli.tracingConfiguration().set(
+            cli.tracingConfiguration().set(
                 new TracingConfigurationCoordinates.Builder(SQL).build(),
                 new TracingConfigurationParameters.Builder()
-                    .withSamplingRate(SAMPLING_RATE_ALWAYS).build());*/
+                    .withSamplingRate(SAMPLING_RATE_ALWAYS).build());
         }
 
         /** */
@@ -140,8 +140,8 @@ public class SqlTracingBenchmarks {
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.SECONDS)
     @Warmup(iterations = 1, time = 10)
-    @Measurement(iterations = 5, time = 10)
-    @Fork(value = 3)
+    @Measurement(iterations = 10, time = 10)
+    @Fork(value = 1)
     public void benchmarkSelect(BenchmarkContext ctx) {
         long lowId = ThreadLocalRandom.current().nextLong(TABLE_POPULATION - SELECT_RANGE);
 
@@ -161,7 +161,7 @@ public class SqlTracingBenchmarks {
     }
 
     /** */
-    @Benchmark
+   /* @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.SECONDS)
     @Warmup(iterations = 1, time = 10)
@@ -183,5 +183,5 @@ public class SqlTracingBenchmarks {
         ) {
             // No-op.
         }
-    }
+    }*/
 }
